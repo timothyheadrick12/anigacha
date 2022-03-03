97b601d6c90f50355e68ccb8a722be976552c7ee
Timothy Headrick <twh53r@umsystem.edu>
2022-03-02 21:08:15 -0600

    fixed description error

--- src/commands/Summon.ts
+++ src/commands/Summon.ts
@@ -1,19 +1,23 @@
-import {BaseCommandInteraction, Client, MessageEmbed} from 'discord.js';
-import {Command} from '../Command';
-import getCharacter from '../requests/getCharacter';
-import Character from '../models/Characters';
+import { BaseCommandInteraction, Client, MessageEmbed } from "discord.js";
+import { Command } from "../Command";
+import getCharacter from "../requests/getCharacter";
+import Character from "../models/Characters";
 
 export const Summon: Command = {
-  name: 'summon',
-  description: 'Summons an anime character',
-  type: 'CHAT_INPUT',
+  name: "summon",
+  description: "Summons an anime character",
+  type: "CHAT_INPUT",
   run: async (client: Client, interaction: BaseCommandInteraction) => {
     const character = await getCharacter();
     const characterEmbed = new MessageEmbed()
-      .setTitle(character.name.full + ' from ' + character.anime)
+      .setTitle(character.name.full + " from " + character.anime)
       .setImage(character.image.medium)
-      .setDescription(character.description ? character.description : '')
-      .addField('Rarity', character.rarity);
+      .setDescription(
+        character.description && character.description.length < 4096
+          ? character.description
+          : ""
+      )
+      .addField("Rarity", character.rarity);
 
     Character.create({
       id: character.id,
